cff698609a8ec8d7192d9c3bbd8c51a51962f34c
Michael <michael@ripper3.aelanflix.vu>
2019-03-26 19:29:02 +1100

    init

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/test">Test</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap';
+@import 'node_modules/bootstrap-vue/src/index.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
